f1bb9a2c28899a99313f19ab7a2fb053bdf2b508
sunyoung <minnie2002@naver.com>
2024-04-02 21:29:31 +0900

    first commit

--- src/test/java/com/example/unitTestStudy/Post/PostTest.java
+++ src/test/java/com/example/unitTestStudy/Post/PostTest.java
@@ -1,2 +1,10 @@
-package com.example.unitTestStudy.Post;public class PostTest {
+package com.example.unitTestStudy.Post;
+
+import org.junit.jupiter.api.Test;
+
+public class PostTest {
+    @Test
+    void Save_User(){
+
+    }
 }
